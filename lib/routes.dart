7b18a7015aedf98c258ec10b8b2b4dee913c7c7b
Savecoders <pablopincay2003@gmail.com>
2026-01-08 00:04:16 -0500

    feat(routes): update routes to use new presentation structure and correct widget references

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,19 @@
-import 'package:autovitae/view/auth_view/login_view/login_view.dart';
-import 'package:autovitae/view/auth_view/register_view/register_view.dart';
+import 'package:autovitae/presentation/auth/pages/login_page.dart';
+import 'package:autovitae/presentation/auth/pages/register_page.dart';
+import 'package:autovitae/presentation/auth/pages/change_password_page.dart';
+import 'package:autovitae/presentation/admin/home_admin_screen.dart';
+import 'package:autovitae/presentation/client/home_client_screen.dart';
+import 'package:autovitae/presentation/manager/home_manager_screen.dart';
+import 'package:autovitae/presentation/client/screens/talleres_disponibles_screen.dart';
 import 'package:flutter/material.dart';
 
 final Map<String, WidgetBuilder> routes = {
-  '/': (context) => const LoginView(),
-  '/registerCliente': (context) => const RegistrarCliente(),
+  '/': (context) => const LoginPage(),
+  '/login': (context) => const LoginPage(),
+  '/registerCliente': (context) => const RegisterPage(),
+  '/cambiar_password': (context) => const ChangePasswordPage(),
+  '/home_admin': (context) => const HomeAdminScreen(),
+  '/home_client': (context) => const HomeClientScreen(),
+  '/home_gerent': (context) => const HomeManagerScreen(),
+  '/talleres_cliente': (context) => const TalleresDisponiblesScreen(),
 };

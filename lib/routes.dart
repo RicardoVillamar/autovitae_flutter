1f9240411230264e1b0799095269fe1cd66cf542
Savecoders <pablopincay2003@gmail.com>
2026-02-01 13:10:14 -0500

    refactor(routes): add routes for editing vehicles and client profiles for improved navigation

--- lib/routes.dart
+++ lib/routes.dart
@@ -9,6 +9,9 @@ import 'package:autovitae/presentation/admin/home_admin_screen.dart';
 import 'package:autovitae/presentation/client/home_client_screen.dart';
 import 'package:autovitae/presentation/manager/home_manager_screen.dart';
 import 'package:autovitae/presentation/client/screens/talleres_disponibles_screen.dart';
+import 'package:autovitae/presentation/client/screens/vehiculo_cliente_screen.dart';
+import 'package:autovitae/presentation/client/screens/edit_profile_cliente_screen.dart';
+import 'package:autovitae/data/models/vehiculo.dart';
 import 'package:flutter/material.dart';
 
 final Map<String, WidgetBuilder> routes = {
@@ -20,9 +23,14 @@ final Map<String, WidgetBuilder> routes = {
   '/home_client': (context) => const HomeClientScreen(),
   '/home_gerent': (context) => const HomeManagerScreen(),
   '/talleres_cliente': (context) => const TalleresDisponiblesScreen(),
-  
   '/create_taller': (context) => const CreateTallerScreen(),
   '/edit_taller': (context) => const EditTallerScreen(),
   '/create_gerente': (context) => const CreateGerenteScreen(),
   '/edit_gerente': (context) => const EditGerenteScreen(),
+  '/create_vehiculo': (context) => const VehiculoClienteScreen(),
+  '/edit_vehiculo': (context) {
+    final vehiculo = ModalRoute.of(context)!.settings.arguments as Vehiculo;
+    return VehiculoClienteScreen(vehiculo: vehiculo);
+  },
+  '/edit_profile_cliente': (context) => const EditProfileClienteScreen(),
 };
